44d8701dd442d33e342e66427af0cde90d59c121
varunpanchal752 <varunpanchal752@gmail.com>
2021-06-30 14:28:46 +0530

    assessmetn

---
+++ AssessmentProject/Backend/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const { response } = require('express');
+
+const port = 3001;
+
+app.use(bodyParser.json());
+
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Role');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    next();
+  })  
+
+app.get('/postsId',(req,res) => {
+    axios(`https://jsonplaceholder.typicode.com/posts/${req.query.id}/comments`)
+    .then((response) => {res.send(JSON.stringify(response.data));} )
+    .catch((err)=>{console.log(err,"Error occured in posts comments")} );
+});
+
+app.get('/posts',(req,res) => {
+    let  respond ;
+    axios('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => {respond = response.data; 
+        res.send(JSON.stringify(respond));} )
+    .catch((err)=>{console.log(err,"Error occured in posts")} );
+});
+
+
+app.listen(port,() => (
+    console.log(`Server is running on ${port}`)
+));
